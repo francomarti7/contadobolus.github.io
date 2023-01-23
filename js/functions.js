27a497c3ba13b1364f37c4a49cbb892fbc5c7f77
unknown <franco_martinez93@otmail.com>
2023-01-23 09:58:21 -0300

    Ajustando detalles

--- js/functions.js
+++ js/functions.js
@@ -12,16 +12,16 @@ function sumar(){
         Swal.fire({
             icon: 'warning',
             title: 'Cuidado',
+            confirmButtonColor:'#00ADB5',
             html:'El nivel de boludos esta subiendo considerablemente! &#x1F628;',
           })
-    } 
-
-   else if (contador%8===0){
+    }  else if (contador%8===0){
         Swal.fire({
             icon: 'warning',
             title: 'ALERTA',
             html: 'Nivel de boludos CRITICO, tomate un descanso&#10071;&#10071; <br> <br> <strong> DESEA RETIRARSE? </strong> ',
             iconColor: '#FF0000',
+            confirmButtonColor: '#00ADB5',
             showCancelButton: true,
             confirmButtonText: 'Me quiero ir',
             cancelButtonText: 'Quiero seguir',
@@ -42,15 +42,41 @@ function sumar(){
 
 function final(){
     var contador = localStorage.getItem("contador") || 0;
-    Swal.fire({
+
+    if (contador == 1){
+      Swal.fire({
         icon: 'success',
         title: 'Felicidades',
-        html: "Hoy viste " +contador+ " boludos! &#128169;",
+        html: "Hoy viste " +contador+ " boludo! &#128169;",
+        confirmButtonColor: '#00ADB5',
       })
+
     contador=0;
     h2.textContent=contador;
     localStorage.setItem("contador", contador);
-};
+    return;
+
+    } else if (contador>1){
+      Swal.fire({
+          icon: 'success',
+          title: 'Felicidades',
+          html: "Hoy viste " +contador+ " boludos! &#128169;",
+          confirmButtonColor: '#00ADB5',
+        })
+
+      contador=0;
+      h2.textContent=contador;
+      localStorage.setItem("contador", contador);
+
+    } else {
+      Swal.fire({
+        icon: 'success',
+        title: 'Felicidades',
+        html: "Hoy no viste boludos! &#128512;",
+        confirmButtonColor: '#00ADB5',
+      })
+    }
+ }
 
 agregar.onclick = sumar;
 fin.onclick = final;
